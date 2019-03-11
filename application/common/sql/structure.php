dd130ef6851e1b2e9dd71103499cec43d1a17404
weiyang <weiyang@vchangyi.com>
2019-03-11 14:22:31 +0800

    修改sql语法错误

--- application/common/sql/structure.php
+++ application/common/sql/structure.php
@@ -12,13 +12,13 @@ return "
       `name` VARCHAR(50) NOT  NULL DEFAULT '' comment '分类名称',
       `description` VARCHAR(255) comment '分类描述',
       `logo` VARCHAR(255) NOT  null DEFAULT '' comment '分类图标',
-      PRIMARY KEY (`id`),
+      PRIMARY KEY (`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='分类表';
     
     CREATE TABLE IF NOT EXISTS `fly_topic` (
       `id` int unsigned not null auto_increment comment '主键',
       `content` text not null DEFAULT '' comment '内容',
-      `modify_time` int not null DEFAULT 0 commnet '修改时间',
+      `modify_time` int not null DEFAULT 0 comment '修改时间',
       `publish_time` int not null DEFAULT 0 comment '发表时间',
       `title` VARCHAR(50) NOT NULL DEFAULT '' comment '帖子标题',
       `category_id` int unsigned not null default 0 comment '所属分类id',
@@ -34,9 +34,9 @@ return "
       `id` int unsigned not null auto_increment comment '主键',
       `modify_time` int unsigned not null default 0 comment '修改时间',
       `publish_time` int unsigned not null default 0 comment '发表时间',
-      `topic_id` int insigned not null default 0 comment '帖子id',
+      `topic_id` int unsigned not null default 0 comment '帖子id',
       `user_id` int unsigned not null default 0 comment '答复人id',
-      `content` text not null default '' commnet '答复内容',
+      `content` text not null default '' comment '答复内容',
       PRIMARY KEY (`id`),
       KEY `topic_id` (`topic_id`),
       KEY `user_id` (`user_id`)
