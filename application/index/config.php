eff32b1157f91be7e82c6a5ffbe624ab47167ce3
weiyang <weiyang@vchangyi.com>
2019-03-26 16:40:37 +0800

    会员注册功能

---
+++ application/index/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ian
+ * Date: 2019/3/11
+ * Time: 3:28 PM
+ */
+return [
+    'view_replace_str' => [
+        '__CSS__' => '/static/index'
+    ]
+];
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,89 @@
 <?php
 namespace app\index\controller;
 
-class Index
+use think\captcha\Captcha;
+use think\Controller;
+use think\Db;
+use think\Exception;
+use think\Log;
+
+class Index extends Controller
 {
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
+
+        return view();
+    }
+
+    public function login()
+    {
+        return view();
+    }
+
+    /**
+     * 注册
+     * @author Ian
+     * @desc
+     * @params
+     * @return
+     * @return \think\response\View
+     */
+    public function register()
+    {
+        if(request() -> isPost()) {
+            //验证码验证
+            $verify = input('post.verify');
+            $captcha = new Captcha();
+            var_dump($captcha -> check($verify,'register'));die;
+            if(!$captcha -> check($verify,'register')) {
+                $this -> error("验证码错误");
+            }
+            //检测手机是否已经被注册
+            $mobile = input('post.mobile');
+            $mobileRes = db('user')
+                -> where('mobile',$mobile)
+                -> find();
+            if(!empty($mobileRes)) {
+                return $this -> error("该手机号已经被注册过");
+            }
+            //写入数据
+            $data = [
+                'nick_name' => input('post.nick_name'),
+                'mobile' => input('post.mobile'),
+                'password' => md5(input('post.password')),
+                'reg_time' => time()
+            ];
+
+            $result = db('user') -> insert($data);
+
+            if($result !== false) {
+                 return $this -> success("注册成功",'Index/index');
+            } else {
+                return  $this -> error("网络异常");
+            }
+
+        }
+        return view();
+    }
+
+    /**
+     * 验证码
+     * @author Ian
+     * @desc
+     * @params
+     * @return
+     * @return \think\Response
+     */
+    public function verify()
+    {
+
+        $verify = new Captcha();
+        $verify -> imageH = 40;
+        $verify -> imageW = 150;
+        $verify -> useNoise = false;
+        $verify -> useCurve = false;
+        $verify -> fontSize = 18;
+        $verify -> length = 4;
+        return $verify -> entry('register');
     }
 }
